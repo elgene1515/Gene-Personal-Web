5bdb1b3a3761564b54dfffd417a93b62edb9eefd
GENE <elgenereyes1515@gmail.com>
2026-02-04 21:10:31 +0800

    fix app bar mobile first responsiveness.

--- src/components/templates/AppBar.vue
+++ src/components/templates/AppBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-app-bar class="appbar-main-container" fluid fixed>
+    <v-app-bar class="appbar-main-container">
         <!-- NOTE Title -->
         <v-app-bar-title class="title" @click="navigationStore.gotoSection('homeRef')">
             Software <span id="title-gene">GENE</span>gineer
@@ -9,7 +9,7 @@
 
         <!-- NOTE Navigation Buttons -->
         <div v-if="isLargeScreen">
-            <ButtonNavigation v-for="(navigation, navigationIndex) in navigations" :key="navigationIndex" :text="navigation.text" @click="navigationStore.gotoSection(navigation.section)"/>
+            <ButtonNavigation class="navigation-button"  v-for="(navigation, navigationIndex) in navigations" :key="navigationIndex" :text="navigation.text" @click="navigationStore.gotoSection(navigation.section)"/>
         </div>
         <v-app-bar-nav-icon v-else @click="drawer = !drawer" :icon="drawer ? 'mdi-close' : 'mdi-menu'" :ripple="false" variant="plain"></v-app-bar-nav-icon>
     </v-app-bar>
@@ -29,11 +29,11 @@
     const navigationStore = useNavigationStore();
     const navigations = ref([
         {
-            text: "Professional Experience",
+            text: "Experience",
             section: "professionalExperience"
         },
         {
-            text: "Software Engineering Projects",
+            text: "Projects",
             section: "softwareEngineeringProjects"
         },
         {
@@ -52,23 +52,50 @@
 
     // NOTE responsive hamburger UI
     const { width } = useDisplay();
-    const isLargeScreen = computed(() => width.value >=1200);
+    const isLargeScreen = computed(() => width.value >=1024);
     const drawer = ref(false);
 </script>
 
 <style lang="scss" scoped>
+    // NOTE Mobile (Portrait)	0px to 480px.
     .appbar-main-container , .navigation-drawer{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
         background-color:  #2C3E50;
         color: cyan;
 
         .title {
             font-weight: bold;
-            font-size: 150%;
             min-width: fit-content;
             cursor: pointer;
             
             #title-gene {
                 color: white;
+            }
+        }
+    }
+
+    // NOTE Tablets	768px	iPads and Android tablets.
+    @media (min-width: 768px) {
+        .appbar-main-container , .navigation-drawer{
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            background-color:  #2C3E50;
+            color: cyan;
+
+            .title {
+                font-weight: bold;
+                min-width: fit-content;
+                cursor: pointer;
+                font-size: 150%;
+                
+                #title-gene {
+                    color: white;
+                }
             }
         }
     }
